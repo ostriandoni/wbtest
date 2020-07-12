0decd119019ebedfc17d1e4d81c8e7ac3644b70f
Donny Ostrianto Sundawa <donny.ostrianto@gmail.com>
2020-07-12 20:02:39 +0700

    fetch and store 10 unique jokes by invoke POST /jokes endpoint

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const axios = require('axios')
 const router = express.Router()
-const BASE_URL = 'http://api.icndb.com/jokes/random/'
+const BASE_URL = 'http://api.icndb.com/jokes'
 const Jokes = require('../models/Jokes')
 
 router.get('/random', async (req, res) => {
   try {
-    const response = await axios.get(BASE_URL)
+    const response = await axios.get(`${BASE_URL}/random`)
 
     if (response && response.data) {
       res.json({ joke: response.data.value.joke })
@@ -39,6 +39,41 @@ router.delete('/', async (req, res) => {
   try {
     await Jokes.deleteMany()
     res.json({ message: 'All jokes has been deleted' })
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Internal server error')
+  }
+})
+
+router.post('/', async (req, res) => {
+  const TOP_TEN = 10
+  const jokes = []
+
+  try {
+    const response = await axios.get(BASE_URL)
+
+    if (response && response.data) {
+      const count = await Jokes.countDocuments()
+
+      for (let i = 0; i < TOP_TEN; i++) {
+        let isExist = await Jokes.findOne({
+          ext_id: response.data.value[count + i].id,
+        })
+
+        if (!isExist) {
+          joke = new Jokes({
+            ext_id: response.data.value[count + i].id,
+            joke: response.data.value[count + i].joke,
+          })
+          jokes.push(joke)
+          await joke.save()
+        }
+      }
+
+      res.json({ jokes })
+    } else {
+      res.status(404).send('No data found')
+    }
   } catch (error) {
     console.error(error.message)
     res.status(500).send('Internal server error')
